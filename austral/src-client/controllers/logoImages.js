e0e890b50db4d919eaff1af86a03ec6a405a7940
mailin <mailinadaromdp@gmail.com>
2023-01-18 22:43:34 -0300

    experiencias en home

--- austral/src-client/controllers/logoImages.js
+++
@@ -1,9 +0,0 @@
-const logoBrands = {
-    'logoFiat': require('../assets/images/logo-fiat.png'),
-    'logoVW': require('../assets/images/vw-logo.png'),
-    'logoNissan': require('../assets/images/nissan-logo.png'),
-    'logoRenault': require('../assets/images/Renault-Logo.png'),
-    'logoChevrolet': require('../assets/images/chevrolet-logo.png')
-};
-
-   
