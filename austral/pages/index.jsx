b06abfbff87304e2165166c91127796d4a54ca4b
mailin <mailinadaromdp@gmail.com>
2023-01-25 18:17:57 -0300

    tag de google search console

--- austral/pages/index.jsx
+++ austral/pages/index.jsx
@@ -6,7 +6,7 @@ import {motion} from 'framer-motion'
 
 
 export const config = {
-  unstable_runtimeJS: false, 
+  unstable_runtimeJS: false,
 }
 
 // const inter = Inter ({ 
@@ -39,6 +39,7 @@ export default function Home() {
         {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Anek+Telugu:wght@100;200;300;400;500;600;700;800&family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/> */}
+      <meta name="google-site-verification" content="GDZpDah58kjFg2Q5RBcgT48NdKk0DkwrQFHB_k8hZkc" />
       </Head>
       <motion.main exit={{color : 'white'}} 
                 initial={{opacity: 0}}
