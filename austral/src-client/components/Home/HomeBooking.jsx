f2f377dce920cd1a25b9d7c040346b7bdcf6d8ad
mailin <mailinadaromdp@gmail.com>
2023-01-28 12:14:25 -0300

    test 3 perfo image

--- austral/src-client/components/Home/HomeBooking.jsx
+++ austral/src-client/components/Home/HomeBooking.jsx
@@ -102,7 +102,7 @@ function HomeBooking () {
      <h1>{t.title.split("\n").map((item, key) => {
       return <span key={key}>{item}<br/></span>
      })}</h1>    
-      <Image src ={HomeMain} alt="familia de vacaciones en patagonia" sizes='(max-width: 768px) 90vw, 100vw' layout="responsive" priority />
+      <Image src ={HomeMain} alt="familia de vacaciones en patagonia" width={1920} height={1080} layout="responsive" objectFit="cover" objectPosition="center" priority/>
     </div>
       <Image className={styles.hexa} src={Hexa} alt="icono de hexagono" />
 
