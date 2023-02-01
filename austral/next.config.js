827619382abc78dd6e302cee25b811e0103208da
mailin <mailinadaromdp@gmail.com>
2023-01-31 22:51:05 -0300

    test nuevo diseno 2

--- austral/next.config.js
+++ austral/next.config.js
@@ -8,29 +8,28 @@ const nextConfig = {
     locales: ['en', 'es'],
   },
   swcMinify: true, 
-    images: {
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-      path: '/_next/image',
-      loader: 'default',
-      loaderFile: 'next/image',
-      disableStaticImages: false,
-      minimumCacheTTL: 60,
-      formats: ['image/webp'],
-      dangerouslyAllowSVG: false,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-      remotePatterns: [],
-      unoptimized: false,
-      domains: ['res.cloudinary.com'],
-  },
+  //   images: {
+  //     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  //     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  //     path: '/_next/image',
+  //     loader: 'default',
+  //     disableStaticImages: false,
+  //     minimumCacheTTL: 60,
+  //     formats: ['image/webp'],
+  //     dangerouslyAllowSVG: false,
+  //     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  //     remotePatterns: [],
+  //     unoptimized: false,
+  //     domains: ['res.cloudinary.com'],
+  // },
 }
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
-//module.exports = nextConfig
+module.exports = nextConfig
 
 
 
-module.exports = withBundleAnalyzer(nextConfig)
+//module.exports = withBundleAnalyzer(nextConfig)
