e4d04bb6927f47d526e670f17343967221963180
Mailin Adaro <mailinadaromdp@gmail.com>
2023-03-29 14:30:18 -0600

    pdf download

--- austral/src-client/components/Home/HomeBooking.jsx
+++ austral/src-client/components/Home/HomeBooking.jsx
@@ -7,12 +7,12 @@ import homeEn from "../../../public/locale/EN/home.json"
 import styles from '../../../styles/Home/HomeBooking.module.css'
 
 function HomeBooking () {
-  const [height, setHeight] = useState("100%");
   const iframeRef = useRef(null);
   const router = useRouter();
   const t = router.locale === "es" ? homeEs : homeEn;
   
   const handleResize = () => {
+    console.log(iframeRef, "iframeRef")
 //     let iframe = document.querySelector("#custom-iframe"); 
 
   
@@ -32,7 +32,7 @@ function HomeBooking () {
 // }, false); 
     // para setear el alto del iframe acorde al contenido dinamico
   
-    console.log(iframeRef.current.contentWindow, "content window") 
+    console.log(iframeRef.current , "iframeRef.current") 
     // como el iframe es de otro dominio no se puede acceder a su contenido directamente
     //console.log(iframeRef.current.contentWindow.document, "document"); // DA UN ERROR DE CORS
     //console.log(iframeRef.current.postMessage) //da undefined
@@ -46,8 +46,7 @@ function HomeBooking () {
       })}</h1>
       <Image src ={HomeHero} sizes= " (max-width: 768px) 200vw, 100vw" placeholder='blur' alt="Familia jugando en una plaza de Bariloche" priority/>
     </div>
-    <iframe  className={styles.booking__all_container} src="https://austral.rentlynetwork.com/widget"  ref={iframeRef} onLoad={handleResize} height={height} scrolling="no"  frameBorder="0" title="Rently Widget"></iframe> 
-    {/* <iframe src="https://rently.rentlynetwork.com/widget" id="custom-iframe"></iframe>  */}
+    <iframe  className={styles.booking__all_container} src="https://austral.rentlynetwork.com/widget"  ref={iframeRef} onLoad={handleResize} scrolling="no"  frameBorder="0" title="Rently Widget"></iframe> 
   </section>
   );
 }
