dd4908aeec1ed29559cd87813f7d4231e286919e
mailin celina adaro marco del pont <mailinadaromdp@gamil.com>
2023-05-05 10:47:21 -0300

    cambio de site url

--- austral/server.js
+++ austral/server.js
@@ -1,59 +1,59 @@
-// const { createServer } = require('http')
-// const { parse } = require('url')
-// const next = require('next')
-// // const cancelFormHandler = require('./api/cancelForm')
-// // const contactFormHandler = require('./api/contactForm')
-// // const newsletterFormHandler = require('./api/newsletterForm')
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+// const cancelFormHandler = require('./api/cancelForm')
+// const contactFormHandler = require('./api/contactForm')
+// const newsletterFormHandler = require('./api/newsletterForm')
 
 
-// const dev = process.env.NODE_ENV !== 'production'
-// const hostname = process.env.NODE_ENV !== 'production' ? 'localhost' : "https://www.patagoniaaustralrentacar.com.ar"
-// const port = process.env.PORT || 3000
-// // when using middleware `hostname` and `port` must be provided below
-// const app = next({ dev, hostname, port })
-// const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = process.env.NODE_ENV !== 'production' ? 'localhost' : process.env.SITE_URL
+const port = process.env.PORT || 3000
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
 
 
-// app.prepare().then(() => {
-//   createServer(async (req, res) => {
-//     try {
-//       // Be sure to pass `true` as the second argument to `url.parse`.
-//       // This tells it to parse the query portion of the URL.
-//       const parsedUrl = parse(req.url, true)
-//       const { pathname, query } = parsedUrl
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
 
 
-//       if (pathname === '/a') {
-//         await app.render(req, res, '/a', query)
-//       } else if (pathname === '/b') {
-//         await app.render(req, res, '/b', query)
-//       } else {
-//         await handle(req, res, parsedUrl)
-//       }
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query)
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query)
+      } else {
+        await handle(req, res, parsedUrl)
+      }
 
-//       // if (pathname === '/api/cancelForm') {
-//       //   await cancelFormHandler(req, res, query)
-//       // } else if (pathname === '/api/contactForm') {
-//       //   await contactFormHandler(req, res, query)
-//       // } else if (pathname === '/api/newsletterForm') {
-//       //   await newsletterFormHandler(req, res, query)
-//       // } else {
-//       //   await handle(req, res, parsedUrl)
-//       // }
+      // if (pathname === '/api/cancelForm') {
+      //   await cancelFormHandler(req, res, query)
+      // } else if (pathname === '/api/contactForm') {
+      //   await contactFormHandler(req, res, query)
+      // } else if (pathname === '/api/newsletterForm') {
+      //   await newsletterFormHandler(req, res, query)
+      // } else {
+      //   await handle(req, res, parsedUrl)
+      // }
 
 
-//     } catch (err) {
-//       console.error('Error occurred handling', req.url, err)
-//       res.statusCode = 500
-//       res.end('internal server error')
-//     }
-//   })
-//     .once('error', (err) => {
-//       console.error(err)
-//       process.exit(1)
-//     })
-//     .listen(port, () => {
-//       console.log(`> Ready on http://${hostname}:${port}`)
-//     })
-// })
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+    .once('error', (err) => {
+      console.error(err)
+      process.exit(1)
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
+})
--- austral/src-client/controllers/sendDataForm.js
+++ austral/src-client/controllers/sendDataForm.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const sendDataForm = async(input) => {
     try {
-      const res = await axios.post("/api/contactForm", input)
+      const res = await axios.post('/api/contactForm', input) 
     } catch (error) {
         console.log(error)
         return error;
